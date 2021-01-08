ccef28f991c6f193e1d0a2fe92b1d64a706a7e6d
Austin Webb <sawebb30@gmail.com>
2021-01-07 20:39:36 -0500

    rm urql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import { withUrqlClient } from "next-urql";
 import Head from "next/head";
 import { useQuery } from "urql";
-import { createUrqlClient } from "../src/utils/createUrqlClient";
 import styles from "../styles/Home.module.css";
 
-const QUERY = `query {
-  mockQuery {
-    id
-    mock
-  }
-}`;
-
-function Home() {
-  const [{ data }] = useQuery({ query: QUERY });
+export default function Home() {
 
   return (
     <div className={styles.container}>
@@ -30,10 +21,7 @@ function Home() {
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
+        
       </main>
 
       <footer className={styles.footer}>
@@ -49,5 +37,3 @@ function Home() {
     </div>
   );
 }
-
-export default withUrqlClient(createUrqlClient, { ssr: false })(Home);
